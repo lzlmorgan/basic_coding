8abefaaff2fa06ea792373efdb13a29b67ff1c75
lzlmorgan <morganliu0314@outlook.com>
2022-06-08 20:43:10 -0400

    3rd version with promotions

--- password_game.py
+++ password_game.py
@@ -1,10 +1,13 @@
 pw = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	password = input('plz type in the password: ')
 	if pw == password:
 		print('you can login')
 		break
 	else:
-		i = i -1
-		print('error password', i , ' time left')
+		if i > 0:
+			print('password error! ', 'You have ', i , ' times left')
+		else:
+			print('you do not have any chance to try')
