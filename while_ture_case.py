9a80b62b84e40144637f8a4c1ecab9a0598a3e76
lzlmorgan <morganliu0314@outlook.com>
2022-06-09 11:12:13 -0400

    while true case

---
+++ while_ture_case.py
@@ -0,0 +1,10 @@
+while True:
+	mode = input ('input game mode: ')
+	if mode == 'q':
+		break
+	elif mode == '1':
+		print('start mode 1')
+	elif mode == '2':
+		print('start mode 2')
+	else:
+		print('only 1/2/q works')
