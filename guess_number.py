fc82ec30f3f628b3d6a5cc7d972d1120292d50c8
lzlmorgan <morganliu0314@outlook.com>
2022-06-08 21:07:23 -0400

    2rd version with time count

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
 	num = int(input('plz guess number: '))
 	if num == r:
 		print('you are right!')
+		print('this is the', count, 'time for you')
 		break
 	elif num > r:
 		print('your answer is bigger!')
 	elif num < r:
 		print('your answer is smaller!')
+	print('this is the', count, 'time for you')
 
