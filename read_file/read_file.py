f340bad588975bffcdad19b8ee90012e47fafea5
lzlmorgan <morganliu0314@outlook.com>
2022-06-10 00:09:01 -0400

    get rid of \n with .strip

--- read_file/read_file.py
+++ read_file/read_file.py
@@ -1,3 +1,9 @@
+data = []
 with open ('food.txt', 'r') as f:
+	#as当作f档案
 	for line in f:
-		print(line)
+		#print(line)
+		data.append(line.strip()) #把每一个line放入data清单list
+		#.strip()去掉换行符\n
+
+	print(data)
