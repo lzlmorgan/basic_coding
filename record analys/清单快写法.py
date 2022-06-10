f4e28473f12475ae608cda6f0daed575ec62abad
lzlmorgan <morganliu0314@outlook.com>
2022-06-10 01:09:49 -0400

    1st

---
+++ record analys/清单快写法.py
@@ -0,0 +1,15 @@
+data = []
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+good =[d for d in data if 'good' in d]
+print(len(good)) 
+
+good = [1 for d in data if 'good' in d] #把1装入清单，当遇见一笔有good的留言
+print(good)
