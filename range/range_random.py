c860c8b269cdb7d7a127fbf38db2678e0b8580e9
lzlmorgan <morganliu0314@outlook.com>
2022-06-11 01:10:27 -0400

    2rd version

--- range/range_random.py
+++ range/range_random.py
@@ -2,7 +2,7 @@ import random
 
 for i in range(3):  #完成三次loop
 	r = random.randint(1,100) #从1-100找到三个随机数
-	print(r)
+	print('this is the ', i + 1, 'time to print random number--', r)
 
 print('-------')
 for n in range(3):
