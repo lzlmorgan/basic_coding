952f047e0bfc7ed24bb16bbdd4ec8642348e5fdc
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 23:53:00 -0400

    1st verison of return function

---
+++ Coding Structure/return.py
@@ -0,0 +1,16 @@
+#return 回传
+#function如果有return，就可以把function执行的结果存下来
+#可以想象function运行后会把【化身】成为return的东西
+
+def add(x, y):
+	return x + y
+result = add(3, 4)
+print(result)
+
+
+
+def average(numbers):
+	avg = sum(numbers) / len(numbers) #除法自动转化为float
+	return avg
+a = average([1, 2, 3])
+print(a)
