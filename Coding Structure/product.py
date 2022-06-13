b787e24f578a7cce0f41b2cdf654e3a917123d18
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 22:13:29 -0400

    2rd final version with 2D list

--- Coding Structure/product.py
+++ Coding Structure/product.py
@@ -4,10 +4,5 @@ while True:
 	if name == 'q':
 		break
 	price = input('please input the current price: ')
-	p = [] #生成一个小list去包涵name和price
-	p.append(name)
-	p.append(price)
-	products.append(p) #将list p 放入products清单
+	products.append([name, price]) #将list p 放入products清单
 print(products)
-
-print(products[0][0]) #在清单中拿出其一个list的第一个值
