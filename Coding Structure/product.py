b4344d2543b3b886a26e8c3cce64c5e544110003
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 22:32:55 -0400

    4th version of writing the info down 'with open'

--- Coding Structure/product.py
+++ Coding Structure/product.py
@@ -5,8 +5,11 @@ while True:
 		break
 	price = input('please input the current price: ')
 	products.append([name, price]) #将list p 放入products清单
-print(products)
 
 for p in products: 
-	print(p) #print出products list中每一个小list p
 	print(p[0], ' is with the price of', p[1]) #print出list p的第一个0和第二个1
+
+
+with open('product.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' +p[1] + '\n') #string合并 write写入文档 \n回车
