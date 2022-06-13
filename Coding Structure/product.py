0586f46180d38e1cf4fb0763900c5f4389072d4e
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 23:12:50 -0400

    6th version with 'continue' function & re-factor the structure

--- Coding Structure/product.py
+++ Coding Structure/product.py
@@ -2,11 +2,13 @@
 products = []
 with open('products.csv', 'r', encoding = 'utf-8') as f:
 	for line in f:
+		if 'product, price' in line:
+			continue #continue跳过回直接到下一回
 		name, price = line.strip().split(',') #strip()去掉\n尾部回车 & split分割数据by comma s是清单
-		products.append([name, price])
+		products.append([name, price]) #把name和price放入products list
 print(products)
 
-
+#让使用者输入product和price
 while True:
 	name = input('please type in the product name: ')
 	if name == 'q':
@@ -15,10 +17,12 @@ while True:
 	price = int(price) #转换price到integer
 	products.append([name, price]) #将list p 放入products清单
 
+#print所有购买记录
 for p in products:
 	print(p[0], ' is with the price of', p[1]) #print出list p的第一个0和第二个1
 
 
+#写入档案
 with open('products.csv', 'w', encoding = 'utf-8') as f: #encoding加入编码规则
 	f.write('product, price\n') #加入column的名字product和price
 	for p in products:
