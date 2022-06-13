3260442536ed9f70e3719d8b1f12d120bca16d4c
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 22:49:55 -0400

    4th version of adding column names 'product' & 'price'

--- Coding Structure/product.py
+++ Coding Structure/product.py
@@ -4,12 +4,14 @@ while True:
 	if name == 'q':
 		break
 	price = input('please input the current price: ')
+	price = int(price) #转换price到integer
 	products.append([name, price]) #将list p 放入products清单
 
-for p in products: 
+for p in products:
 	print(p[0], ' is with the price of', p[1]) #print出list p的第一个0和第二个1
 
 
-with open('product.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f: #encoding加入编码规则
+	f.write('product, price\n') #加入column的名字product和price
 	for p in products:
-		f.write(p[0] + ',' +p[1] + '\n') #string合并 write写入文档 \n回车
+		f.write(p[0] + ',' + str(p[1]) + '\n') #string合并 write写入文档 \n回车
