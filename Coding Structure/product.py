4ee5ec8b7beb12ec10d7dcc2265e024a2e7b3bc2
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 22:21:43 -0400

    3rd version of adding for loop to print product'name and price

--- Coding Structure/product.py
+++ Coding Structure/product.py
@@ -6,3 +6,7 @@ while True:
 	price = input('please input the current price: ')
 	products.append([name, price]) #将list p 放入products清单
 print(products)
+
+for p in products: 
+	print(p) #print出products list中每一个小list p
+	print(p[0], ' is with the price of', p[1]) #print出list p的第一个0和第二个1
