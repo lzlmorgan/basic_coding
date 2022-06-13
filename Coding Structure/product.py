67c3f80af9203ff16c5f6986fbf1401b82560e94
lzlmorgan <morganliu0314@outlook.com>
2022-06-13 00:23:55 -0400

    8th version of the refactor

--- Coding Structure/product.py
+++ Coding Structure/product.py
@@ -1,36 +1,53 @@
+#function只做一件事
 import os #operating system作业系统import
 
-
-products = []
-#检查档案是否存在
-if os.path.isfile('products.csv'): #os.path.isfile调用os插件功能来判断文件是否存在
-	print('yeah, the file exist!!!')
-	#读取档案
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
+#读取档案
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding = 'utf-8') as f:
 		for line in f:
 			if 'product, price' in line:
 				continue #continue跳过回直接到下一回
 			name, price = line.strip().split(',') #strip()去掉\n尾部回车 & split分割数据by comma s是清单
 			products.append([name, price]) #把name和price放入products list
-	print(products)
-else:
-	print('the file dose not exist!!!')
+	return products
 
 #让使用者输入product和price
-while True:
-	name = input('please type in the product name: ')
-	if name == 'q':
-		break
-	price = input('please input the current price: ')
-	price = int(price) #转换price到integer
-	products.append([name, price]) #将list p 放入products清单
+def user_input(products):
+	while True:
+		name = input('please type in the product name: ')
+		if name == 'q':
+			break
+		price = input('please input the current price: ')
+		price = int(price) #转换price到integer
+		products.append([name, price]) #将list p 放入products清单
+	print(products)
+	return products
 
 #print所有购买记录
-for p in products:
-	print(p[0], ' is with the price of', p[1]) #print出list p的第一个0和第二个1
+def print_products(products):
+	for p in products:
+		print(p[0], 'is with the price of', p[1]) #print出list p的第一个0和第二个1
 
 #写入档案
-with open('products.csv', 'w', encoding = 'utf-8') as f: #encoding加入编码规则
-	f.write('product, price\n') #加入column的名字product和price
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n') #string合并 write写入文档 \n回车
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f: #encoding加入编码规则
+		f.write('product, price\n') #加入column的名字product和price
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n') #string合并 write写入文档 \n回车
+
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('yeah, the file exist!!!')
+		products = read_file(filename)
+	else:
+		print('the file dose not exist!!!')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+
+main()
