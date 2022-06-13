55383f3fc492ce144c3e570a9708e2bfa49e391c
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 23:45:29 -0400

    2nd version of function's parameter introduction

--- Coding Structure/function.py
+++ Coding Structure/function.py
@@ -2,14 +2,33 @@
 #function用来收纳程式代码
 #它个功能
 
-def wash(): #定义function def=define
-	print('add water')
+def wash(dry=False, water = 8): #定义function def=define def需要设计投币口
+	print('add water', water, 'level')
 	print('add cleaner')
 	print('spin')
+	if dry: #if dry is true
+		print('dry')
+wash()
+print('-----------')
+wash(water = 10)
+print('-----------')
+wash(True) #使用function
+print('-----------')
+wash(False)
 
-wash() #使用function
-
+print('----------')
 def say_hi():
 	print('hi')
-
 say_hi()
+print('-----------')
+
+def add(x = 0, y = 0): #parameter可以设定预设值 有预设值不用强制设定token
+	print(x + y)
+add(5) #5默认给x
+print('-----------')
+add(y = 5) #设置y = 5
+print('-----------')
+add(3, 4)
+print('------------')
+add(123, 333)
+print('------------')
