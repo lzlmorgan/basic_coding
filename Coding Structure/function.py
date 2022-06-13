127734a89cb86f69bb3e0d16049019d85601700d
lzlmorgan <morganliu0314@outlook.com>
2022-06-12 23:28:42 -0400

    1st version: function introdution

---
+++ Coding Structure/function.py
@@ -0,0 +1,15 @@
+#function 函式/功能
+#function用来收纳程式代码
+#它个功能
+
+def wash(): #定义function def=define
+	print('add water')
+	print('add cleaner')
+	print('spin')
+
+wash() #使用function
+
+def say_hi():
+	print('hi')
+
+say_hi()
