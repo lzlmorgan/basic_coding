d56d52a9f64b52e5bfc8c145169f1721dcaf432a
lzlmorgan <morganliu0314@outlook.com>
2022-06-14 22:40:08 -0400

    final version

--- dictionary.py
+++ dictionary.py
@@ -9,3 +9,18 @@ words['tea'] = '茶' #增加新的key和value
 print(words)
 print(words['ramen'])
 print(words['pasta'])
+
+
+p0 = {
+	'name' : 'ramen',
+	'price' : '100'
+}
+
+p1 = {
+	'name' : 'ramen',
+	'price' : '50'
+}
+
+drinks = [p0, p1]
+print(drinks[0]['name'])
+print(drinks[1]['price'])
