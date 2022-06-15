38818bf65b88eebcfa38e62a63178b82537c7f12
lzlmorgan <morganliu0314@outlook.com>
2022-06-14 23:24:23 -0400

    final version with dictionary

--- record analys/word_count.py
+++ record analys/word_count.py
@@ -9,24 +9,24 @@ with open ('reviews.txt', 'r') as f:
 			print(len(data))
 print('档案读取完成，一共有', len(data),'笔资料')
 
-sum_len = 0
-for d in data: 
-	sum_len = sum_len + len(d) #初始长度为0，赋值每一笔长度到新的长度，求和
-	#print(sum_len)
-print('平均长度', sum_len/len(data)) #len(data)
-
-new = []
-for d in data: #d是data留言list中每一笔的留言
-	if len(d) < 100:
-		new.append(d)
-print(len(new), '笔留言长度小于100')
-print(new[0]) #打出第一笔长度小于100的留言
-print(new[1]) #打出第二笔长度小于100的留言
-
-good = []
+wc = {} #word count
 for d in data:
-	if 'good' in d:
-		good.append(d)
-print(len(good), '笔留言中含有good')
-print(good[0]) #打印第一条含有good的留言
-print(good[1])
+	words = d.strip().split() #split预设值直接去掉空白
+	for word in words:
+		if word in wc:
+			wc[word] += 1 #已知key+1
+		else:
+			wc[word] = 1 #新增key进字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+while True:
+	word = input('plz type the word: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print('your key word shows', wc[word], 'times')
+	else:
+		print('No such word in reviews')
