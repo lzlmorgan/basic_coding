dd8089e0a53dc814c949208216bd04ab4b0f6442
lzlmorgan <morganliu0314@outlook.com>
2022-06-14 22:34:51 -0400

    2rd version on add element of dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,11 @@
 #dictionary 
 
 words = {
-	'ramen' : '拉麵',
+	'ramen' : '拉麵',  #key : value
 	'pasta' : '義大利麵'
 }
 
+words['tea'] = '茶' #增加新的key和value
+print(words)
 print(words['ramen'])
 print(words['pasta'])
